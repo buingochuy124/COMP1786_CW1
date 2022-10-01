56cb6d78a8d2928d287a5997e0895704ea53a961
Bui Ngoc Huy <buingochuy124@gmail.com>
2022-10-02 01:54:42 +0700

    init spinner feature C

---
+++ app/src/main/java/com/example/tripdiary/ExpensesActivity.java
@@ -0,0 +1,14 @@
+package com.example.tripdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpensesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses);
+    }
+}
